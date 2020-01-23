51786dc98c62a86ae3db1759dbbc8063c66404ab
Quisqueyan <stanley.sanchez86@gmail.com>
2020-01-23 13:52:51 -0800

    added picture as result of prompt

--- javascript.js
+++ javascript.js
@@ -1,2 +1,31 @@
-alert('are you ready to shred');
+// alert('are you ready to shred');
 
+/*function good() {
+var question =  prompt('are you ready to shred')
+alert('Great');
+
+confirm("do you love the freedom of snow boarding?");
+
+
+
+if(confirm === true) {
+  alert("that's awesome");
+}else{
+  SpeechRecognitionAlternative("thats too bad")
+}
+}*/
+
+var showOrder = function() {
+  var order = prompt('are you ready to shred');
+  var item;
+
+  if (order === 'yes') {
+    item = '<img src="images/jorg-angeli.jpg">';
+  } else if (order === 'no') {
+    item = '<img src="images/robson.jpg">';
+  } else{
+    item = '<img src="images/keep your secrets.jpg">';
+  }
+
+  return item;
+}
